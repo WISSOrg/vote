f4df510655bac2f8d7d16564e1cada1002869aa8
Jun Kato <i@junkato.jp>
2017-12-05 22:17:49 +0900

    Add admin authorization

--- routes/admin.js
+++ routes/admin.js
@@ -2,38 +2,17 @@ var _ = require('lodash');
 var express = require('express');
 var router = express.Router();
 
-/* Top page */
-router.get('/', function(req, res, next) {
-  if (res.locals.user) {
-    return res.redirect('/vote');
+router.use((req, res, next)=>{
+  if (!res.locals.user || !res.locals.user.isCommittee) {
+    return res.status(403).json({'error': 'you are not a committee member.'});
   }
-  res.render('index', { title: 'Express' });
+  next();
 });
 
-// router.get('/put', function(req, res, next) {
-//   if (!req.query
-//       || !req.query.id
-//       || !req.query.name
-//       || !req.query.vote) {
-//     res.json({"error": "insufficient query parameter"});
-//     return;
-//   }
-//   if (isNaN(parseInt(req.query.id))
-//       || isNaN(parseInt(req.query.vote))) {
-//     res.json({"error": "incorrect query parameter"});
-//     return;
-//   }
-//   var entry = {
-//       "id": parseInt(req.query.id)
-//     , "name": req.query.name
-//     , "vote": parseInt(req.query.vote)
-//   };
-//   var db = req.db;
-//   var votes = db.collection('votes');
-//   votes.insertOne(entry).then(function (result) {
-//     res.json({"result": result, "entry": entry});
-//   });
-// });
+/* Top page */
+router.get('/', function(req, res, next) {
+  res.redirect('/');
+});
 
 router.get('/get/all', function(req, res, next) {
   var db = req.db;
