673746fa48d8b0c2810e03f9a41b62cb4e128f65
Jun Kato <i@junkato.jp>
2017-12-05 21:12:08 +0900

    Add touch support; add client-side vote check

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -1,6 +1,6 @@
 
 /* toggle checkbox when list group item is clicked */
-$('.list-group a').click(function(e){
+$('.list-group a').on('click touch', function(e){
   e.stopPropagation();
   var $a = $(this)
     , $this = $a.find("[type=checkbox]");
@@ -8,7 +8,16 @@ $('.list-group a').click(function(e){
     $a.removeClass('checked');
     $this.prop("checked",false);
   } else {
+    if (countChecked() >= 3) return;
     $a.addClass('checked');
     $this.prop("checked",true);
   }
 });
+
+function countChecked() {
+  var checked = 0;
+  $('.list-group a input[type=checkbox]').each((i,e)=>{
+    if ($(e).prop("checked")) checked ++;
+  });
+  return checked;
+}
