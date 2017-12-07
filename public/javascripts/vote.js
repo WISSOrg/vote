0b3d95f49fbb42b4ab45b8197ea8968499372035
Jun Kato <i@junkato.jp>
2017-12-07 18:22:40 +0900

    Support ticking checkboxes with checkboxes in anchors

--- public/javascripts/vote.js
+++ public/javascripts/vote.js
@@ -1,9 +1,22 @@
 
 /* toggle checkbox when list group item is clicked */
 $('.list-group a').on('click touch', function(e){
+  var input = e.target.tagName.toLowerCase() === 'input';
   e.stopPropagation();
   var $a = $(this)
     , $this = $a.find("[type=checkbox]");
+  if (input) {
+    if($this.is(":checked")) {
+      if (countChecked() > 3) {
+        $this.prop("checked",false);
+        return;
+      }
+      $a.addClass('checked');
+    } else {
+      $a.removeClass('checked');
+    }
+    return;
+  }
   if($this.is(":checked")) {
     $a.removeClass('checked');
     $this.prop("checked",false);
